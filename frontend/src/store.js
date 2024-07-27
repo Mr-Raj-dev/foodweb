ef0d9b2ad19ac89720196141d49ba93e1f324146
rajchaudar <rajchaudar60@gmail.com>
2024-07-27 18:54:45 +0530

    "Added Redux to Home component and wrapped App with Provider in index.js"

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import {
+    legacy_createStore as createStore,
+    combineReducers,
+    applyMiddleware,
+    compose,
+} from "redux";
+import thunk from "redux-thunk";
+import { restaurantReducer } from "./reducer/restaurantReducer";
+
+const reducer = combineReducers({
+    restaurants: restaurantReducer,
+});
+
+const composeenhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ ||
+    compose;
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    composeenhancers(applyMiddleware(...middleware))
+);
+
+export default store; 
